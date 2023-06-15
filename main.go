b8932abbb8ac410270f3ab6486e4fbfe66027093
Diaz Harizky Firdaus <diazharizky@gmail.com>
2023-06-15 12:34:47 +0700

    Replace sql with nosql

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/diazharizky/go-graphql-bootstrap/internal/app"
 	"github.com/diazharizky/go-graphql-bootstrap/internal/repositories"
 	"github.com/diazharizky/go-graphql-bootstrap/internal/server"
-	"github.com/diazharizky/go-graphql-bootstrap/pkg/db"
+	"github.com/diazharizky/go-graphql-bootstrap/pkg/mongodb"
 )
 
 func main() {
 	appCtx := app.NewContext()
 
-	dbConn := db.GetConnection()
+	db, err := mongodb.GetDB()
+	if err != nil {
+		log.Fatalf("error unable to get database: %v", err)
+	}
 
-	appCtx.UserRepository = repositories.NewUserRepository(dbConn)
-	appCtx.TodoRepository = repositories.NewTodoRepository(dbConn)
+	appCtx.UserRepository = repositories.NewUserRepository(db)
+	appCtx.TodoRepository = repositories.NewTodoRepository(db)
 
 	server.Run(appCtx)
 }
